74854925849e7148c1d25e34d8238c351abeab48
Soufiane Boutallaka <s.boutallaka@gmail.com>
2019-05-17 15:46:26 +0200

    firt bootstrap

--- app.js
+++ app.js
@@ -5,7 +5,8 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var morningRouter =require('./routes/matin');
+var eveningRouter =require('./routes/soir');
 
 var app = express();
 
@@ -20,7 +21,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/matin', morningRouter);
+app.use('/soir', eveningRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
