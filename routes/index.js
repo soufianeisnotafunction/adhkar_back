7b260155c4f546e488136a2da39764ec6c895636
Soufiane Boutallaka <sboutallaka@octo.com>
2020-05-08 16:22:06 +0200

    Revert "updated title"
    This reverts commit d507fe4039e3c13d3ea6dff33b425c7eab2a70cd.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Adhkar deployed with github actions', subtitle:'Invocations du matin et du soir' });
+  res.render('index', { title: 'Adhkar', subtitle:'Invocations du matin et du soir' });
 });
 
 module.exports = router;
